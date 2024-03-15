618bab6771fb4cd4cb60024936024b231d2e8527
Gahbriehel <babatise002@gmail.com>
2024-03-14 21:03:34 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     b = int(argv[3])
     operator = argv([2])
     if operator == '+':
-        print(f"{a} {operator} {b} = {add(a, b)}")
+        print("{} + {} = {}".format(a, b, add(a, b)))
     elif operator == "-":
         print(f"{a} {operator} {b} = {sub(a, b)}")
     elif operator == "*":
