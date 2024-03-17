693192b7b1dfe74e0244f65f122ce39a19eeb480
Gahbriehel <babatise002@gmail.com>
2024-03-17 07:58:48 -0700

    function that prints all integers of a list

---
+++ 0x03-python-data_structures/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import math
+import random
+# my_list = [x for x in range(0, random(0, 9))]
+# print(my_list)
+user_input = input("Enter Miles: ")
+answer = float(user_input) * 1.60934
+print(f"{user_input} miles = {answer} kilometers")
