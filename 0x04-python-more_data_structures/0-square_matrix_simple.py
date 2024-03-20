39960f07e960a736654b6aedac386c221d0edbd8
Gahbriehel <babatise002@gmail.com>
2024-03-20 15:29:45 -0700

    function that replaces all occurrences of an element by another in a new list

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for num in matrix:
-        new_matrix.append(list(map(lambda x : x * x, num)))
+        new_matrix.append(list(map(lambda x: x * x, num)))
     return new_matrix
