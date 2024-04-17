d33e5e07916a6f1d149fa0182682d92eb1dcba5c
Gahbriehel <babatise002@gmail.com>
2024-04-16 19:37:27 -0700

    function to read utf-8 text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+
+"""Defines a filename read_file function"""
+
 def read_file(filename=""):
+    """prints the file being read"""
+
     with open(filename, encoding="utf-8") as my_file:
         print(my_file.read())
