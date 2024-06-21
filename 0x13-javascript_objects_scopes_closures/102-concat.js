408b1c573d304f0046752cf13c88a80add71ec47
Gahbriehel <babatise02@gmail.com>
2024-06-21 16:10:58 -0700

    10-converter.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,14 +5,19 @@ const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 const destination = process.argv[4];
 
-const file1 = fs.readFile('arg1', (err, data) => {
-	if (err) throw err;
-}
-const file2 = fs.readFile('arg2', (err, data) => {
+let file1 = fs.readFile('arg1', (err, data) => {
+	if (err) {
+		throw (err);
+	}
+})
+let file2 = fs.readFile('arg2', (err, data) => {
         if (err) throw err;
-};
-
+});
+console.log(file1);
+/*
+file1.toString();
+file2.toString();
 result = file1 + file2;
 fs.writeFile('destination', result, (err) => {
 	if (err) throw err;
-}
+})
