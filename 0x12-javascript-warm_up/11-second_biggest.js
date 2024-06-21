e4e1a0a5bd1a458efeb037cd4e897fb95e7a40b5
Gahbriehel <babatise02@gmail.com>
2024-06-21 15:39:09 -0700

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,9 +7,9 @@
  */
 const args = process.argv.slice(2).map(Number);
 if (args.length === 0 || args.length === 1) {
-	console.log(0);
+  console.log(0);
 } else {
-	const max = Math.max(...args);
-	const secondBiggest = Math.max(...args.filter(x => x < max));
-	console.log(secondBiggest);
+  const max = Math.max(...args);
+  const secondBiggest = Math.max(...args.filter(x => x < max));
+  console.log(secondBiggest);
 }
