b5ce0aa4544f4aee683505fa0c8e1c9164987db6
Gahbriehel <babatise002@gmail.com>
2024-06-25 08:08:12 -0700

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This lists all states with a name starting with N (upper N) 
+This lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa
 """
 from sys import argv
