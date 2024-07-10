0e61d56ed3b9514953b27793e253f008137c2aff
Gahbriehel <babatise002@gmail.com>
2024-07-09 19:48:02 -0700

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,7 +4,8 @@
  + found in the header of the response. """
 import urllib.request
 import sys
-url = sys.argv[1]
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as res:
-    print(dict(res.headers).get('X-Request-Id'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as res:
+        print(dict(res.headers).get('X-Request-Id'))
