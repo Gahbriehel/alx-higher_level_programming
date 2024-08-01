f8f134b6d6767476fa85a94978f26ff4c713d68e
Gahbriehel <babatise002@gmail.com>
2024-08-01 09:01:20 -0700

    2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/*
+ * This script display the status code of a GET request
+ *
+ * The first argument is the URL to request (GET)
+ * The status code must be printed like this: |code: <status code>|
+ * The module request is used
+ *
+ * Test with:
+ * 	./2-statuscode.js https://alx-intranet.hbtn.io/status
+ * code: 200
+ * 	./2-statuscode.js https://alx-intranet.hbtn.io/doesnt_exist
+ * code: 404
+ *
+ */
+
+const request = require('request');
+const url = process.argv[2];
+request(url, function (err, res, body) {
+  console.log('code: ', res.statusCode);
+});
