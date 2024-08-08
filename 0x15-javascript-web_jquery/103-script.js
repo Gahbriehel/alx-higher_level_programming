181fadfbe39fe0a22eb0ce26b8d4a00e7d45f346
Gahbriehel <babatise002@gmail.com>
2024-08-07 21:15:00 -0700

    103-script.js

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+  const button = $('INPUT#btn_translate');
+  const input = $('INPUT#language_code');
+
+  function printLanguage () {
+    const value = input.val();
+
+    if (value) {
+      const uri = 'https://hellosalut.stefanbohacek.dev/?lang=' + value;
+
+      $.ajax({
+        url: uri,
+        success: function (result) {
+          const helloTranslation = $('DIV#hello');
+
+          helloTranslation.text(result.hello);
+        }
+      });
+    }
+  }
+
+  button.on('click', printLanguage);
+  input.on('keypress', function (event) {
+    if (event.key === 'Enter') {
+      printLanguage();
+    }
+  });
+};
